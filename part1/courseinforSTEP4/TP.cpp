08b7284968100bb6c891c96f294dee46dda0d6f9
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-04-12 20:59:49 +0300

    Add files via upload

---
+++ part1/courseinforSTEP4/TP.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define For(type, i, a, b) for(type i = a; i <= b; i++)
+ 
+typedef long long ll;
+ 
+const ll MOD = 1e9 + 7;
+ 
+const ll limit = 1e5 + 5;
+ 
+ll test;
+ll a;
+ll x, y;
+
+ 
+void FastIn(){
+    ios_base::sync_with_stdio(false);
+    cout.tie(0);
+    cin.tie(0);
+}
+void ReadIn(){
+ 
+}
+ 
+int main(){
+    cin >> x >> y;
+    
+    FastIn();
+    ReadIn();
+}
