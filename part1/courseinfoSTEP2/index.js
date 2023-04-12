7c68f6f81d65202dc78cc4cf7b279a518a4b6a27
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-04-12 21:21:37 +0300

    Step 2

--- part1/courseinfoSTEP2/index.js
+++ part1/courseinfoSTEP2/index.js
@@ -3,4 +3,4 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
