f24b0f4cad6cf4e0567eae21647e61152764b850
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-04-12 21:22:23 +0300

    Step 1

--- part1/courseinfoSTEP1/App.js
+++ part1/courseinfoSTEP1/App.js
@@ -17,7 +17,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-      <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p> Number of exercises {props.exercises1 + props.exercises2 + props.exercises3} </p>
 
     </div>
   )
