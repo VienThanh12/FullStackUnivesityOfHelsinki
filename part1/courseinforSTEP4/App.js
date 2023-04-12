6b5b6f42f75610e2650f4f3646ce0717b69af890
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-04-12 21:00:19 +0300

    Add files via upload

---
+++ part1/courseinforSTEP4/App.js
@@ -0,0 +1,51 @@
+const Header = (props) => {
+  return (
+    <div>
+      <h1> {props.course} </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p> {props.parts[0].name} {props.parts[0].exercises} </p>
+      <p> {props.parts[1].name} {props.parts[1].exercises} </p>
+      <p> {props.parts[2].name} {props.parts[2].exercises} </p>
+    </div>
+  )
+}
+const Total = (props) => {
+  return (
+    <div>
+      <p> Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  )
+}
+export default App
