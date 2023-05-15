e2ade2609df766796782d3a0695f9907617bc964
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-05-15 10:54:56 +0700

    Add files via upload

---
+++ part1/courseinforSTEP11/App.js
@@ -0,0 +1,63 @@
+import { useState } from 'react'
+
+const Button = ({label, value}) => {
+  return (
+    <button onClick = {label}> {value} </button>
+  )
+}
+const StatisticLine = ({label, value}) => {
+  return (
+  <table>
+    <tbody>
+    <tr>
+      <td> {label} </td>
+      <td> {value} </td>
+    </tr>
+    </tbody>
+  </table>
+  )
+}
+
+// a proper place to define a component
+const Statistics = ({good, neutral, bad}) => {
+  var total = good + neutral + bad
+  var total_point = good - bad
+  var positive_num = good / total * 100
+  
+  if(total == 0)
+    return <StatisticLine label = "No feedback given"/>
+  else if(total > 0)
+    return (
+      <div>
+        <StatisticLine label = "good" value = {good}/>
+        <StatisticLine label = "neutral" value = {neutral}/>
+        <StatisticLine label = "bad" value = {bad}/>
+        <StatisticLine label = "all" value = {good + neutral + bad}/>
+        <StatisticLine label = "average" value = {total_point / total}/>
+        <StatisticLine label = "positive" value = {`${good / total * 100} %`}  /> 
+      </div>
+    )
+}
+
+const App = () => {
+ 
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const increaseGoodByOne = () => setGood(good + 1)
+  const increaseNeutralByOne = () => setNeutral(neutral + 1)
+  const increaseBadByOne = () => setBad(bad + 1)
+  const Feedback = () => <Statistics good = {good} bad = {bad} neutral = {neutral}/>
+  return (
+    <div>
+      <h1> give feedback </h1>
+      <Button label = {increaseGoodByOne} value = "good"/>
+      <Button label = {increaseNeutralByOne} value = "neutral"/>
+      <Button label = {increaseBadByOne} value = "bad"/>
+      <h1> statistics </h1>
+      <Statistics good ={good} neutral = {neutral} bad = {bad}/>
+    </div>
+  )
+}
+
+export default App
