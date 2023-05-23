c0501d8df3dd989a19831f312a5710283999bc7d
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-05-23 15:40:46 +0700

    Add files via upload

---
+++ part2/courseinforSTEP8/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+	public String name;
+	public int age;
+	public float height;
+	
+	public Person(String name, int age, float height){
+		this.name = name;
+		this.age = age;
+		this.height = height;
+	}
+
+}
