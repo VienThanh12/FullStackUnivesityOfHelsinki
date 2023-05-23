f2f9adaeae73948453ff9e02ff55a05bddea72fe
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-05-23 14:48:01 +0700

    Create node.js

---
+++ Draft/node.js
@@ -0,0 +1,15 @@
+import Note from './Note'
+
+const App = ({ notes }) => {
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
+}
+export default App
