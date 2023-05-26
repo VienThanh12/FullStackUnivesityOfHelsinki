a0b0843d8163404ea4c5d88858214de818968b41
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-05-26 10:55:47 +0700

    Step 8

---
+++ part2/PhonebookSTEP3/App.js
@@ -0,0 +1,56 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { 
+      name: 'Arto Hellas',
+      num: '040-1234567'
+    }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+  const addNote = (event) =>{
+    event.preventDefault()
+    const NameObject = {
+      name: newName,
+      num: newNum
+    }
+    setPersons(persons.concat(NameObject))
+    setNewName('')
+    setNewNum('')
+  }
+  const handPersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handNumberChange = (event) => {
+    setNewNum(event.target.value)
+  }
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit = {addNote}>
+        <div>
+          name: <input 
+          onChange = {handPersonChange}
+          />
+        </div>
+        <div> 
+          number: <input
+          onChange = {handNumberChange}
+          />
+        </div>  
+        <div>
+          <button type = "submit"> add </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <p>
+        {
+          persons.map(persons => <p> {persons.name} {persons.num}</p>)
+        }
+      </p>
+    </div>
+  )
+}
+
+export default App
