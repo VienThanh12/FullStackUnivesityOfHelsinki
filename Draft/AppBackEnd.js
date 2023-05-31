c0efe4f3fa32236729859cdcea6ec91996b34474
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-05-31 11:15:56 +0700

    Fetching Date From Server

---
+++ Draft/AppBackEnd.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+  console.log('render', notes.length, 'notes')
+
+  // ...
+}
