f8a6647f0a77c6479f978d22d4185760d238846c
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-05-31 13:29:44 +0700

    Step 11

---
+++ part2/PhonebookSTEP6/App.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+  
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes')
+  console.log(notes)
+  if(notes.length > 0){
+    return (
+      <Note note = {notes} />
+    )
+  }
+  // ...
+}
+export default App
