20771745f8af4ab1045c31b83af262faba2fcf36
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-06-04 13:52:24 +0700

    Step 13

---
+++ part2/PhonebookSTEP8/App.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import personService from './services/persons'
+const Filter = ({handFilterNameChange}) => {
+  return (
+    <form>
+        <div>
+          filter shown with <input
+          onChange = {handFilterNameChange}
+          />
+        </div>
+    </form>
+  )
+}
+
+const PersonForm = ({addNote, handPersonChange, handNumberChange}) => {
+  return(
+    <form onSubmit = {addNote}>
+      <div>
+        name: <input 
+        onChange = {handPersonChange}
+        />
+      </div>
+      <div> 
+        number: <input
+        onChange = {handNumberChange}
+        />
+      </div>  
+      <div>
+        <button type = "submit"> add </button>
+      </div>
+    </form>
+  )
+}
+
+const Persons = ({persons, personsToShow}) => {
+  return (
+    <p>
+        {
+          personsToShow.map(persons => <p key = {persons.id} > {persons.name} {persons.number}</p>)
+        }
+    </p>
+  )
+}
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNum, setNewNum] = useState('')
+  const [filterName, setFilterName] = useState('')
+  
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(initialPersons => {
+        setPersons(initialPersons)
+      })
+  }, [])
+
+  const personsToShow = persons.filter(persons => persons.name.includes(filterName.toLowerCase()))
+  
+  const addNote = (event) =>{
+    event.preventDefault()
+    const NameObject = {
+      name: newName,
+      num: newNum,
+      id: persons.length + 1
+    }
+    setPersons(persons.concat(NameObject))
+    setNewName('')
+    setNewNum('')
+    setFilterName('')
+  }
+  const handPersonChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handNumberChange = (event) => {
+    setNewNum(event.target.value)
+  }
+  const handFilterNameChange = (event) => {
+    setFilterName(event.target.value) 
+  }
+  return (
+    <div>
+      <h2> Phonebook </h2>
+      <Filter handFilterNameChange = {handFilterNameChange}/>
+      <h3>add a new</h3>
+
+      <PersonForm addNote={addNote} handPersonChange = {handPersonChange} handNumberChange = {handNumberChange}/>
+      <h3>Numbers</h3>
+      <Persons persons = {persons} personsToShow = {personsToShow} />
+    </div>
+  )
+}
+
+export default App
