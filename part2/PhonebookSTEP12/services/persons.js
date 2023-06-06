9054929e21cbc2e809574a65342a0f07eb7057e7
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-06-06 14:19:03 +0700

    Step 17

---
+++ part2/PhonebookSTEP12/services/persons.js
@@ -0,0 +1,24 @@
+import axios from 'axios'
+const baseURL = 'http://localhost:3001/persons'
+
+const getAll = () => {
+    const request = axios.get(baseURL)
+    return request.then(response => response.data)
+}
+
+const create = (newObject) => {
+    const request = axios.post(baseURL, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseURL}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+const remove = (id) => {
+    //console.log(id)
+    const request = axios.delete(`${baseURL}/${id}`)
+    return request.then(response => response.data)
+}
+
+export default { getAll, create, update, remove}   
