ef81e5ec66ffac3bae8bf5d0b7399fcd4e424830
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-06-07 15:50:19 +0700

    Step 18

---
+++ part2/DataForCountriesSTEP1/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom/client'
+import axios from 'axios'
+//import './index.css'
+import App from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+
+
+//ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
