463aedb9b91b68fa998beb20a6beb65d59534c6e
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-06-26 15:38:49 +0700

    Note 3.13

---
+++ Draft/Backend(Note)/models/note.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+
+const url = process.env.MONGODB_URI
+
+
+console.log('connecting to', url)
+
+mongoose.connect(url)
+
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+// format the object returned by Mongoose
+// transform _id to id and delete __v
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+
+module.exports = mongoose.model('Note', noteSchema)
