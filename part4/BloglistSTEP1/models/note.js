a610c5d76b93c41a3f3193fbb2eafddb2a677b3b
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-07-05 11:11:55 +0700

    Step 1

---
+++ part4/BloglistSTEP1/models/note.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+    important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
