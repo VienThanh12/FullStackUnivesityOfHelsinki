0ff2c290b438c3c19fda85f966e5b865b78589d2
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-07-06 16:19:37 +0700

    Step 6

---
+++ part4/HelperFunctionsAndUnitTestsSTEP4/list_helper.js
@@ -0,0 +1,63 @@
+
+const dummy = (blogs) => {
+    return 1
+}
+
+const totalLikes = (listWithOneBlog) => {
+    var sum = 0
+    //console.log(listWithOneBlog[].likes)
+    for(var i = 0; i < listWithOneBlog.length; i++){
+        sum += listWithOneBlog[i].likes
+    }
+    console.log(sum)
+    return sum
+}
+
+const favoriteBlog = (blogs) => {
+    const maxBlog = {
+        title: "",
+        author: "",
+        likes: 0
+    }
+    var maxLikes = 0
+    
+    for(var i = 0; i < blogs.length; i++){
+        if(maxLikes < blogs[i].likes){
+            maxLikes = blogs[i].likes
+            maxBlog.title = blogs[i].title
+            maxBlog.author = blogs[i].author
+            maxBlog.likes = blogs[i].likes
+        }    
+    }
+    return maxBlog
+}
+
+const mostBlog = (blogs) => {
+    const mostBlog = {
+        author: "",
+        blogs: 0
+    }
+    var maxBlogs = 0
+    var a = []
+    for(var i = 0; i < blogs.length; i++)
+        a[blogs[i].author] = 0
+    for(var i = 0; i < blogs.length; i++){
+        a[blogs[i].author] += 1
+        maxBlogs = Math.max(maxBlogs, a[blogs[i].author])
+    }
+
+    for(var i = 0; i < blogs.length; i++){
+        if(maxBlogs === a[blogs[i].author]){
+            mostBlog.author = blogs[i].author
+            mostBlog.blogs = maxBlogs
+        }
+    }
+    return mostBlog
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog,
+    mostBlog
+}
