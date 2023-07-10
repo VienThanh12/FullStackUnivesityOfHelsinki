c08ac99f2932022fcbd2835df71e9cfa321e790b
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-07-10 16:42:41 +0700

    Step 9

---
+++ part4/BlogListTestSTEP2/controllers/blogs.js
@@ -0,0 +1,15 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const blog = new Blog(request.body)
+
+    const result = await blog.save()
+    response.status(201).json(result)
+})
+module.exports = blogsRouter
