1897bf3c2bdd2eebb47774d05b9713950d1fd80f
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-07-10 16:25:23 +0700

    Step 8

---
+++ part4/BlogListTestSTEP1/tests/blog_api.test.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+
+const app = require('../app')
+
+const api = supertest(app)
+// test blog app (retrieve the list of data from testBlogApp)
+
+test('blogs are returned as json', async () => {
+    await api 
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/)
+}, 100000)
+test('there is 2 blog', async () => {
+    const response = await api.get('/api/blogs')
+    //console.log(response.body)
+    expect(response.body).toHaveLength(2)
+})
+afterAll(async () => {
+    await mongoose.connection.close()
+})
