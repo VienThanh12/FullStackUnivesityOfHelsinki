b10a8872fede7630a2e004d7eda74c9683e96699
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-07-12 11:08:50 +0700

    Delete part4/BlogListTestSTEP6 directory

--- part4/BlogListTestSTEP6/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
-
-app.listen(config.PORT, () => {
-    logger.info(`Server running on port ${config.PORT}`)
-})
