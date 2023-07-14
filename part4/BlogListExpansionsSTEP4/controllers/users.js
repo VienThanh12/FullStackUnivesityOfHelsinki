20dfed7956d6fad1078229c9c1e24bd90d994aae
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-07-14 15:03:00 +0700

    Step 16

---
+++ part4/BlogListExpansionsSTEP4/controllers/users.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    response.json(users)
+})
+
+usersRouter.post('/', async (request, response) => {
+    const { username, name, password } = request.body
+
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    if(!password){
+        return response.status(400).json({
+            error: 'password missing'
+        })
+    }
+    if(password.length <= 2){
+        return response.status(400).json({
+            error: 'password is shorter than the minimum allowed length'
+        })
+    }
+    const user = new User({
+        username,
+        name,
+        passwordHash,
+    })
+
+    const savedUser = await user.save()
+
+    response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
