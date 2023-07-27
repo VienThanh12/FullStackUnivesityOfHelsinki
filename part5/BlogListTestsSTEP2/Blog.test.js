e29a062fb7d6b077a42c5a173584f06a0490ac49
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-07-27 10:13:51 +0700

    Step 14

---
+++ part5/BlogListTestsSTEP2/Blog.test.js
@@ -0,0 +1,53 @@
+import React from "react"
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
+
+test('render title and author', () => {
+    const blog = {
+        title: "Fullstack",
+        author: "Kevin",
+        user: {
+            name: "Kevin"
+        }
+    }
+
+    const { container } = render(<Blog blog = {blog}/>)
+    //screen.debug()
+    const div = container.querySelector('.blog')
+        expect(div).toHaveTextContent(
+            'Fullstack Kevin'
+        )
+})
+
+test('clicking the button calls event handler once', async () => {
+    const blog = {
+        title: "Fullstack",
+        author: "Kevin",
+        user: {
+            name: "Kevin"
+        },
+        url: 'https://github.com',
+        likes: 32
+    }
+
+    const { container } = render(
+        <Blog blog = {blog}/>
+    )
+
+    const user = userEvent.setup()
+    const button = screen.getByText('view')
+    await user.click(button)
+    screen.debug()
+    const div = container.querySelector('.view')
+    expect(div).toHaveTextContent(
+        "https://github.com"
+    )
+    expect(div).not.toHaveStyle('display: none')
+    expect(div).toHaveTextContent(
+        "32"
+    )
+    //console.log(div)
+    
+})
