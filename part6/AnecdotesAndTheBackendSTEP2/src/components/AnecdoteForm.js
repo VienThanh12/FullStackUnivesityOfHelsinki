add977fc58d5405a702ec461436372aed3a2e62f
Kevin <67015555+VienThanh12@users.noreply.github.com>
2023-09-03 14:56:54 +0300

    Step 15

---
+++ part6/AnecdotesAndTheBackendSTEP2/src/components/AnecdoteForm.js
@@ -0,0 +1,33 @@
+import { useDispatch } from "react-redux"
+import { addAnecdote , createAnecdote} from '../reducers/anecdoteReducer'
+import { notiCreate, reset5 } from '../reducers/notiReducer'
+import anecdoteService from '../services/anecdotes'
+
+const NewAnecdote = () => {
+    const dispatch = useDispatch()
+
+    const createContent = async (event) => {
+        event.preventDefault()
+        const content = event.target.newContent.value
+        event.target.newContent.value = ''
+        dispatch(addAnecdote(content))
+        dispatch(notiCreate(content))
+        setTimeout(() => {
+            dispatch(reset5())
+        }, 5000)
+        const NewAnecdote = await anecdoteService.createAnecdote(content)
+        dispatch(createAnecdote(NewAnecdote))
+
+    }
+    return (
+        <div>
+            <h2> create new </h2>
+            <form onSubmit = {createContent}>
+                <div> <input name = "newContent"/> </div>
+                <button type = "submit"> create </button>
+            </form>
+        </div>
+    )
+}
+
+export default NewAnecdote
---
+++ part6/AnecdotesAndTheBackendSTEP2/src/reducers/anecdoteReducer.js
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const getId = () => (100000 * Math.random()).toFixed(0)
+
+const anecdoteSlice = createSlice({
+  name: 'Anecdotes',
+  initialState: [],
+  reducers: {
+    createAnecdote(state, action) {
+      state.push(action.payload)
+    },
+    addAnecdote(state, action) {
+      const content = action.payload
+      state.push({
+        content,
+        votes: 0,
+        id: getId(),
+      })
+    },
+    addVote(state, action) {
+      const id = action.payload
+      const anecdoteToChange = state.find(n => n.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote => 
+        anecdote.id !== id ? anecdote : changedAnecdote 
+      ) 
+    },
+    setAnecdotes(state, action) {
+      return action.payload
+    }
+  },
+})
+
+export const { addAnecdote, addVote , setAnecdotes, createAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
