05d18436931feafc866bb614a65778bc54cd3e42
Kevin <67015555+VienThanh12@users.noreply.github.com>
2024-01-15 22:57:10 +0200

    Step 5

---
+++ part7/RoutedAnecdotesSTEP5/src/hooks/index.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+  const reset = () => {
+    setValue('')
+  }
+
+  return {
+    type,
+    value,
+    onChange,
+    reset
+  }
+}
+
+// modules can have several named exports
+export const useAnotherHook = () => {
+  // ...
+}
